24504962204ecf15df085fb499bb58503f59840f
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-04-30 10:33:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,7 @@
 def speak_to_grandma(string)
   if string == string.upcase
     puts "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
