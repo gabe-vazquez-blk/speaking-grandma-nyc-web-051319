e6deaae5eb77518501efcc13f773182b9fb69bda
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-04-30 10:39:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,4 +12,5 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(str)
-  if str == 
+  if str == "I LOVE YOU GRANDMA!"
+    puts ""
